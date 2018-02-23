c5a38ac2b09ac1f9950aa4ceee4952be45df2f12
prasannagonative <venkyprasanna77@gmail.com>
2018-02-23 13:44:05 +0530

    Update set3.py

--- codekata/beginner/set3.py
+++ codekata/beginner/set3.py
@@ -2,9 +2,9 @@ def main():
     pass
 i=raw_input("Enter the letter:")
 if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
-    print "it is a vowel"
+    print "vowel"
 
 else:
-    print "it is a consonent"
+    print "consonent"
 if __name__ == '__main__':
     main()

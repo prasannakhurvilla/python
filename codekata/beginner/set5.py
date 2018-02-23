d2e73b1ec0ff181870655729d792e97f017eaaf2
prasannagonative <venkyprasanna77@gmail.com>
2018-02-23 14:54:33 +0530

    Create set5.py

---
+++ codekata/beginner/set5.py
@@ -0,0 +1,21 @@
+def main():
+    pass
+a=input("Enter a:")
+b=input("Enter b:")
+c=input("Enter c:")
+if a>b:
+    i=a
+else:
+    i=b
+if b>c:
+    j=b
+else:
+    j=c
+if i>j:
+    k=i
+else:
+    k=j
+print k
+
+if __name__ == '__main__':
+    main()

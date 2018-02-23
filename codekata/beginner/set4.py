6d696f80f0b1b2e5f3d70aa25029393cfb1f0552
prasannagonative <venkyprasanna77@gmail.com>
2018-02-23 13:56:17 +0530

    Create set4.py

---
+++ codekata/beginner/set4.py
@@ -0,0 +1,9 @@
+def main():
+    pass
+a=raw_input("Enter the input:")
+if((a>="a" and a<="z") or(a>="A" and a<="Z")):
+	print ("alphabet")
+else:
+	print ("No")
+if __name__ == '__main__':
+    main()

3134b31ad3b71e261361e5e91b10996b2b897bf8
prasannagonative <venkyprasanna77@gmail.com>
2018-02-23 12:12:42 +0530

    Create set1.py

---
+++ codekata/beginner/set1.py
@@ -0,0 +1,11 @@
+def main():
+    pass
+a=int(input("Enter the number:"))
+if (a==0):
+    print "given number is zero"
+elif (a<0):
+    print "given number is negative"
+else:
+    print "given number is positive"
+if __name__ == '__main__':
+    main()

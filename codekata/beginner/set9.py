361f13f4c77247de62c9d23ee1bea9993ed7611c
prasannakhurvilla <36752601+prasannakhurvilla@users.noreply.github.com>
2018-03-14 16:16:02 +0530

    Update set9.py

--- codekata/beginner/set9.py
+++ codekata/beginner/set9.py
@@ -1,9 +1,11 @@
+y=input("Enter the Length of the array:")
 x=input("Input:")
-if(x<1 or (x%1)!=0):
-    print("this is not a valid input")
-else:
-    a=0
-    while(x!=0):
-        a=a+x
-        x=x-1
-    print "Output:",a
+if y<=x:
+    if(x<1 or (x%1)!=0):
+        print("this is not a valid input")
+    else:
+        a=0
+        while(x!=0):
+            a=a+x
+            x=x-1
+        print "Output:",a

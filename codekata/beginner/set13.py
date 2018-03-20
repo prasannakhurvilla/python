27175d6f19765dc320e6ff2bfa37e49f08e467d9
prasannakhurvilla <36752601+prasannakhurvilla@users.noreply.github.com>
2018-03-20 16:02:18 +0530

    Create set13.py

---
+++ codekata/beginner/set13.py
@@ -0,0 +1,11 @@
+num =input("Input:")
+if num > 1:
+   for i in range(2,num):
+       if (num % i) == 0:
+           print("no")
+      
+           break
+   else:
+       print("yes")
+else:
+   print("no")
